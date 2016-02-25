f86aaece31521a4b7019cc65a360d565802fd0c5
Stijn van Schooten <svsoke@hotmail.com>
2016-02-25 17:50:07 +0100

    CORS!

--- server/app.js
+++ server/app.js
@@ -16,6 +16,12 @@ app.use(express.static('user'));
 app.use("/operator", express.static('operator'));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 (function makeConn() {
 	db = mysql.createConnection({
 		host: 'mysql.eu.dualdev.com',
